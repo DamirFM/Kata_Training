017f69454ba1371d3379d133d5528b2b66a68a5d
Damir Yakupov <101779464+DamirFM@users.noreply.github.com>
2022-08-03 00:11:57 -0400

    Delete recursion.test.js

--- recursion.test.js
+++
@@ -1,8 +0,0 @@
-describe('summation', function () {
-    it('should return the correct total', function () {
-      Test.assertEquals(summation(1), 1)
-      Test.assertEquals(summation(8), 36)
-    })
-  })
-  
-  
