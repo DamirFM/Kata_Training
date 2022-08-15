359e49d0e9daa9e06609f25ba8215410b3bfaa12
Damir Yakupov <yakupovdamir93@gmail.com>
2022-08-14 23:32:21 -0400

    add list.js

--- list/list.js
+++ list/list.js
@@ -1,11 +1,11 @@
-const min = function(list){
-    return list.reduce(function(previous,current){ 
-                       return previous < current ? previous:current
-                    });
- }
+function min(list) {
+   return list.reduce(function (previous, current) {
+      return previous < current ? previous : current;
+   });
+}
  
- const max = function(list){
-    return list.reduce(function(previous,current){ 
-                       return previous > current ? previous:current
-                    });
- }
+ function max(list) {
+   return list.reduce(function (previous, current) {
+      return previous > current ? previous : current;
+   });
+}
