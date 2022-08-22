b3318c9f814498ba055f9cdacae3f79fad233e9e
Damir Yakupov <yakupovdamir93@gmail.com>
2022-08-22 00:06:58 -0400

    add comments

--- reverseWords/reverseWords.js
+++ reverseWords/reverseWords.js
@@ -5,3 +5,15 @@
 //"This is an example!" ==> "sihT si na !elpmaxe"
 //"double  spaces"      ==> "elbuod  secaps"
 
+function reverseWord (str) {
+    let i = str.length - 1;
+    let result = "";
+for (i >= 0; str.length - 1; i -= 1) {
+    result += str[i];
+}
+return result.split(" ").reverse().join("");
+}
+
+//Идея состоит в том, чтобы сначала преобразовать строку в массив символов, а затем вызвать Array.prototype.reverse() метод для реверсирования массива на месте. Наконец, соедините массив обратно в строку с помощью Array.prototype.join() метод.
+
+//Чтобы преобразовать строку в массив символов, вы можете использовать любой из следующих методов.
