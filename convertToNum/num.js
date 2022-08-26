3e931f77d43e580f6d100d7173c714bc7cd4d5ba
Damir Yakupov <yakupovdamir93@gmail.com>
2022-08-25 23:46:08 -0400

    add convert

---
+++ convertToNum/num.js
@@ -0,0 +1,11 @@
+//We need a function that can transform a string into a number.
+
+//"1234" --> 1234
+//"605"  --> 605
+//"1405" --> 1405
+//"-7" --> -7
+
+const stringToNumber = function(str){
+    const numbers = Number(str);
+    return numbers;
+  }
